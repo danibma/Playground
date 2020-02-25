6ea600d2dede92ca46c33757141597640b7e0c5c
Daniel Assuncao <daniel.assuncao9@gmail.com>
2020-02-25 01:39:12 +0000

    Added a window and added vulkan to the project

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,62 @@
+#include <assert.h>
 #include <stdio.h>
 
-#include <glm/glm.hpp>
+#include <glfw/glfw3.h>
+#include <vulkan/vulkan.h>
+
+#define WIDTH 1280
+#define HEIGHT 720
+
+#define VK_CHECK(call) \
+	do { \
+		VkResult result_ = call; \
+		assert(result_ == VK_SUCCESS); \
+	} while(0)
 
 int main() 
 {
-	printf("Hello Skel\n");
+	//TODO: Log this asserts and make the glfwInit an if
+	int rc = glfwInit();
+	assert(rc);
 
-	return 0;
+	VkApplicationInfo appInfo = { VK_STRUCTURE_TYPE_APPLICATION_INFO };
+	appInfo.apiVersion = VK_API_VERSION_1_2;
+
+	VkInstanceCreateInfo instanceCreateInfo = { VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO };
+	instanceCreateInfo.pApplicationInfo = &appInfo;
+
+#ifdef _DEBUG
+	const char* debugLayers[] = 
+	{
+		"VK_LAYER_LUNARG_standard_validation"
+	};
+
+	instanceCreateInfo.ppEnabledLayerNames = debugLayers;
+	instanceCreateInfo.enabledLayerCount = sizeof(debugLayers) / sizeof(debugLayers[0]);
+#endif
+
+	const char* extensions[] =
+	{
+		VK_KHR_SURFACE_EXTENSION_NAME
+	};
+
+	instanceCreateInfo.ppEnabledExtensionNames = extensions;
+	instanceCreateInfo.enabledExtensionCount = sizeof(extensions) / sizeof(extensions[0]);
+
+	VkInstance instance = 0;
+	VK_CHECK(vkCreateInstance(&instanceCreateInfo, 0, &instance));
+
+
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Skel Engine", 0, 0);
+	assert(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	vkDestroyInstance(instance, 0);
 }
